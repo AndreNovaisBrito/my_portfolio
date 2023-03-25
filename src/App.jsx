2c6491c2a0dfa19c7f5af5bdbde861d5c4fe6fe6
AndreNovaisBrito <andrenoz@gmail.com>
2023-03-24 21:59:33 -0300

    A lot of work in the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./components/header/Header.tsx";
+import Header from "./components/header/Header";
 import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
 import Portfolio from "./components/portfolio/Portfolio";
@@ -7,7 +7,7 @@ import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import Services from "./components/services/Services";
-import Nav from "./components/nav/Nav.tsx";
+import Nav from "./components/nav/Nav";
 
 const App = () => {
   return (
