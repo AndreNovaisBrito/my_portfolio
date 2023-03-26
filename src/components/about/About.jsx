1a0af3d63b5cc849f03e10e7507090c92c0a9258
AndreNovaisBrito <andrenoz@gmail.com>
2023-03-26 13:48:37 -0300

    about jsx done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me.png";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+
 export const About = () => {
   return (
-    <section id='about'>About</section>
-  )
-}
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
 
-export default About
+      <div className="container about__container">
+        <div className="about_me-image">
+          <img src={ME} alt="About image" />
+        </div>
 
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1 Year Working</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed Projects</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
+            impedit nam, non veritatis ullam, consequuntur dolor, reiciendis
+            rerum fugit magnam enim officia vero blanditiis libero eos
+            laboriosam magni repellendus. Minima.
+          </p>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
