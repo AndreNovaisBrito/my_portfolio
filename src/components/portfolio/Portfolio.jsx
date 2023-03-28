d8096acd8e550cfddd10f6443325dcdb8aa5286d
AndreNovaisBrito <andrenoz@gmail.com>
2023-03-27 21:37:08 -0300

    testimonials done, need to generate reviews dinamically

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -10,46 +10,46 @@ const data = [
   {
     id: 1,
     image: IMG1,
-    title: 'Project 1',
-    github:'https://github.com',
-    demo:'https://github.com'
+    title: "Project 1",
+    github: "https://github.com",
+    demo: "https://github.com",
   },
   {
     id: 2,
     image: IMG1,
-    title: 'Project 1',
-    github:'https://github.com',
-    demo:'https://github.com'
+    title: "Project 1",
+    github: "https://github.com",
+    demo: "https://github.com",
   },
   {
     id: 3,
     image: IMG1,
-    title: 'Project 1',
-    github:'https://github.com',
-    demo:'https://github.com'
+    title: "Project 1",
+    github: "https://github.com",
+    demo: "https://github.com",
   },
   {
     id: 4,
     image: IMG1,
-    title: 'Project 1',
-    github:'https://github.com',
-    demo:'https://github.com'
+    title: "Project 1",
+    github: "https://github.com",
+    demo: "https://github.com",
   },
   {
     id: 5,
     image: IMG1,
-    title: 'Project 1',
-    github:'https://github.com',
-    demo:'https://github.com'
+    title: "Project 1",
+    github: "https://github.com",
+    demo: "https://github.com",
   },
   {
     id: 6,
     image: IMG1,
-    title: 'Project 1',
-    github:'https://github.com',
-    demo:'https://github.com'
-  }
-]
+    title: "Project 1",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+];
 const Portfolio = () => {
   return (
     <section id="portfolio">
@@ -57,35 +57,29 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-
-      {
-        data.map(({id, image, title, github, demo})=>{
+        {data.map(({ id, image, title, github, demo }) => {
           return (
             <article key={id} className="portfolio__item">
-            <div className="portfolio__item-image">
-              <img src={image} alt={title} />
-            </div>
-            <h3>{title}</h3>
-            <div className="porfolio__item-cta">
-              <a href={github} className="btn">
-                Github
-              </a>
-              <a
-                href={demo}
-                className="btn btn-primary"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Demo
-              </a>
-            </div>
-          </article>
-          )
-        })
-      }
-
-
-
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="porfolio__item-cta">
+                <a href={github} className="btn">
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,64 @@
 import React from "react";
 import "./testimonials.css";
+import AVTR1 from "../../assets/me.png";
 
 const Testimonials = () => {
-  return <section id='testimonials'>Testimonials</section>;
+  return (
+    <section id="testimonials">
+      <h5>Review From Clients</h5>
+      <h2>Testimonials</h2>
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar one" />
+          </div>
+          <h5 className="client__name">Ernest Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores
+            doloribus perspiciatis asperiores distinctio numquam nihil
+            praesentium sed obcaecati fugiat cumque eos doloremque vitae
+            impedit, esse dolorum nesciunt atque voluptate aut.
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar one" />
+          </div>
+          <h5 className="client__name">Ernest Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores
+            doloribus perspiciatis asperiores distinctio numquam nihil
+            praesentium sed obcaecati fugiat cumque eos doloremque vitae
+            impedit, esse dolorum nesciunt atque voluptate aut.
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar one" />
+          </div>
+          <h5 className="client__name">Ernest Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores
+            doloribus perspiciatis asperiores distinctio numquam nihil
+            praesentium sed obcaecati fugiat cumque eos doloremque vitae
+            impedit, esse dolorum nesciunt atque voluptate aut.
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar one" />
+          </div>
+          <h5 className="client__name">Ernest Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores
+            doloribus perspiciatis asperiores distinctio numquam nihil
+            praesentium sed obcaecati fugiat cumque eos doloremque vitae
+            impedit, esse dolorum nesciunt atque voluptate aut.
+          </small>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
